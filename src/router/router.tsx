20da3db545ab772506134102112e2351337705d7
Lunas322 <fpvmxm333477@gmail.com>
2026-05-31 23:05:22 +0900

    feat: router 추가 login,signup

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../pages/Login";
+import Signup from "../pages/signup";
 
 export const router = createBrowserRouter([{
-}])
+    path: '/login',
+    element: <Login/>
+},
+{
+    path: '/signup',
+    element: <Signup/>
+}
+])
