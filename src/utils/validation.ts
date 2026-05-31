923c3185e88cf371122f4d0f2895edcea091d8ad
Lunas322 <fpvmxm333477@gmail.com>
2026-06-01 08:13:25 +0900

    feat: validation util 분리

---
+++ src/utils/validation.ts
@@ -0,0 +1,12 @@
+import type { User } from "../types/authType";
+
+export function validation(userData: User, rePassword: string) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const passwordRegex = /^.{6,}$/;
+  return {
+    email: !userData.email || !emailRegex.test(userData.email),
+    password: !userData.password || !passwordRegex.test(userData.password),
+    userName: !userData.userName,
+    rePassword: userData.password !== rePassword,
+  };
+}
