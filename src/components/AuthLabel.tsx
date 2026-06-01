544987d43621861053727e2db8f860cbf1252b52
Lunas322 <fpvmxm333477@gmail.com>
2026-06-01 12:13:50 +0900

    fix: input,label id 추가 및 htmlFor 추가

--- src/components/AuthLabel.tsx
+++ src/components/AuthLabel.tsx
@@ -5,15 +5,23 @@ type AuthLabelProps = {
   error: boolean;
   errorMessage: string;
   children: ReactNode;
+  htmlFor: string;
 };
 
-function AuthLabel({ label, error, errorMessage, children }: AuthLabelProps) {
+function AuthLabel({
+  label,
+  error,
+  errorMessage,
+  children,
+  htmlFor,
+}: AuthLabelProps) {
   return (
     <>
       <div>
-        <label className="mb-2 block text-sm font-medium">{label}</label>
+        <label className="block text-sm font-medium mb-2" htmlFor={htmlFor}>
+          {label}
+        </label>
         {children}
-
         {error && <p className="mt-1 text-sm text-red-500">{errorMessage}</p>}
       </div>
     </>
