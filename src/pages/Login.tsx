4b29ed2b355dee90c7ec6f13b19f556ef8d9376e
Lunas322 <fpvmxm333477@gmail.com>
2026-06-01 12:51:00 +0900

    feat: router 추가 및 navigate 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -37,7 +37,7 @@ function Login() {
         login.email,
         login.password,
       );
-      alert("로그인 성공!");
+      nav('/')
     } catch {
       alert("이메일 또는 비밀번호가 올바르지 않습니다");
     }
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import Home from "../pages/Home";
 
 export const router = createBrowserRouter([{
     path: '/login',
@@ -9,5 +10,8 @@ export const router = createBrowserRouter([{
 {
     path: '/signup',
     element: <Signup/>
+}, {
+    path: "/",
+    element:<Home/>
 }
 ])
