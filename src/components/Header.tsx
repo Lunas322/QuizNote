60c949943c1cacc407021211c7e88f2182fd9b90
Lunas322 <fpvmxm333477@gmail.com>
2026-06-03 18:50:45 +0900

    feat: logout 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { logout } from "../util/logout";
+
+
 function Header() {
+  const nav = useNavigate()
+  const handelLogout = async ()=>{
+    await logout
+    nav('/login')
+  }
   return (
     <div className="w-full border-b bg-white">
       <div className="flex w-full items-center justify-between px-6 py-4">
 
-        <div className="text-xl font-extrabold text-blue-600 transition hover:opacity-80">
+        <div className="text-xl font-extrabold text-blue-600 transition hover:opacity-80" onClick={()=>nav('/')}>
           📚 QuizNote
         </div>
 
@@ -15,7 +24,8 @@ function Header() {
         </div>
 
         <div>
-          <button className="rounded-lg bg-blue-500 px-4 py-2 text-sm font-semibold text-white transition hover:bg-blue-600">
+          <button className="rounded-lg bg-blue-500 px-4 py-2 text-sm font-semibold text-white transition hover:bg-blue-600"
+          onClick={()=>handelLogout()}>
             로그아웃
           </button>
         </div>
