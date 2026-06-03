62b0e4c5cc5ea601751e8bb8155d350452a61882
Lunas322 <fpvmxm333477@gmail.com>
2026-06-03 20:19:37 +0900

    feat: header navigate 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,8 +17,8 @@ function Header() {
         </div>
 
         <div className="flex items-center gap-8 text-sm font-medium text-gray-600">
-          <div className="cursor-pointer transition hover:text-blue-500">문제 생성</div>
-          <div className="cursor-pointer transition hover:text-blue-500">문제 풀이</div>
+          <div className="cursor-pointer transition hover:text-blue-500" onClick={()=>nav('/')}>문제 생성</div>
+          <div className="cursor-pointer transition hover:text-blue-500" onClick={()=>nav('/testselect')}>문제 풀이</div>
           <div className="cursor-pointer transition hover:text-blue-500">결과</div>
           <div className="cursor-pointer transition hover:text-blue-500">마이페이지</div>
         </div>
