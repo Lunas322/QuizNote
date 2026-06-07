71441544efc59611031de0bbf77f67e58e9bc6b3
Lunas322 <fpvmxm333477@gmail.com>
2026-06-07 19:49:37 +0900

    fix: Exam 타입 수정 (ExamData 추가)

--- src/types/examType.ts
+++ src/types/examType.ts
@@ -1,4 +1,11 @@
 export type ExamQuestion = {
+  id: string;
+  uid: string;
+  title: string;
+  examData: ExamData[];
+};
+
+export type ExamData = {
   question: string;
   options: string[];
   answer: number;
