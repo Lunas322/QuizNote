dc447e723cfc10fb7fa15a2c55534ed9d06dc854
Lunas322 <fpvmxm333477@gmail.com>
2026-06-10 00:53:58 +0900

    feat: result 타입 분리

--- src/types/resultType.ts
+++ src/types/resultType.ts
@@ -2,6 +2,10 @@ export type UserAnswer = {
   answer: number;
   question: string;
   questionIndex: number;
+  correctAnswer: number;
+  explanation: string;
+  selectedOption: string;
+  correctOption: string;
 };
 
 export type ResultData = {
