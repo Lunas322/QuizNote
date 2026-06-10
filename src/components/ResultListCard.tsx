995a17fcc5e75965091325dc3b3081a81e0df511
Lunas322 <fpvmxm333477@gmail.com>
2026-06-10 21:41:07 +0900

    refactor: router 경로 통일화 resultList

--- src/components/ResultListCard.tsx
+++ src/components/ResultListCard.tsx
@@ -32,7 +32,7 @@ function ResultListCard ({id,title,createdAt,score}:ResultListCardProps) {
               </div>
 
               <button
-                onClick={() => nav(`/resultlist/${id}`)}
+                onClick={() => nav(`/resultList/${id}`)}
                 className="rounded-xl bg-blue-500 px-5 py-2 text-sm font-semibold text-white transition hover:bg-blue-600"
                 >
                 결과 보기
