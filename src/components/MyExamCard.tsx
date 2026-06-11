79f2355acc4c2cf2617937bc65b40e0556ff63f7
Lunas322 <fpvmxm333477@gmail.com>
2026-06-11 14:32:44 +0900

    feat: MyExamCard 컴포넌트 분리

---
+++ src/components/MyExamCard.tsx
@@ -0,0 +1,36 @@
+import type { Timestamp } from "firebase/firestore"
+import { useNavigate } from "react-router-dom"
+
+type MyExamCardProps = {
+    examId: string
+    title: string
+    createdAt: Timestamp
+}
+
+function MyExamCard ({examId,title,createdAt}:MyExamCardProps) {
+    const nav = useNavigate()
+    return(
+                  <div
+            key={examId}
+            className="flex items-center justify-between rounded-2xl border border-gray-100 p-5 transition hover:bg-gray-50"
+          >
+            <div>
+              <h3 className="font-bold text-gray-800">
+                {title}
+              </h3>
+
+              <p className="text-sm text-gray-500">
+                {createdAt?.toDate().toLocaleDateString()}
+              </p>
+            </div>
+
+            <button
+              className="rounded-xl bg-blue-500 px-4 py-2 text-sm font-semibold text-white hover:bg-blue-600"
+              onClick={() => nav(`/result/${examId}`)}
+            >
+              보기
+            </button>
+          </div>
+    )
+}
+export default MyExamCard
